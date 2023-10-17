f78d3ea6e0db07cee12223b7cdfbc56d933ecde0
jefferson298 <jeffersonmichael211@gmail.com>
2023-10-16 18:44:17 -0700

    task 0

--- printf.c
+++ printf.c
@@ -7,42 +7,41 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
+	int count = 0;
 
-    int count = 0;
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++; /* Move past '%'*/
+			if (*format == '\0')
+			{
+				break; /* End of string after '%'*/
+			}
+			if (*format == 'c')
+			{
+				int c = va_arg(args, int);
 
-    va_start(args, format);
+				putchar(c);
+				count++;
+			} else if (*format == 's')
+			{
+				char *s = va_arg(args, char*);
 
-    while (*format) {
-        if (*format == '%') {
-            format++; /* Move past '%'*/
-            if (*format == '\0') {
-                break; /* End of string after '%'*/
-            }
-
-            if (*format == 'c') {
-                int c = va_arg(args, int);
-                putchar(c);
-                count++;
-            } else if (*format == 's') {
-                char *s = va_arg(args, char*);
-                while (*s) {
-                    putchar(*s);
-                    count++;
-                    s++;
-                }
-            } else if (*format == '%') {
-                putchar('%');
-                count++;
-            } else {
-                /* Unsupported conversion specifier, ignore*/
-            }
-        } else {
-            putchar(*format);
-            count++;
-        }
-        format++;
-    }
-
-    va_end(args);
-    return count;
+				while (*s)
+					putchar(*s);
+					count++;
+					s++;
+			} else if (*format == '%')
+			{
+				putchar('%');
+				count++;
+			}
+		} else
+			putchar(*format);
+			count++;
+		 format++;
+	} va_end(args);
+	return (count);
 }
