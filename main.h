0a5cda6d1cfe0ef6ec40e700c8d10a4186552dd3
jefferson298 <jeffersonmichael211@gmail.com>
2023-10-17 17:24:29 -0700

    main.h

--- main.h
+++ main.h
@@ -1,8 +1,10 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int _printf(const char *format, ...);
 
